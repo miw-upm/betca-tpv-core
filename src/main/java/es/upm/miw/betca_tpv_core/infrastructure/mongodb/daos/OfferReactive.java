0d53ebaf7a745472972617c7c2186c47397576e1
acalvom <andreac.3108@gmail.com>
2021-03-05 13:21:30 +0100

    create Reactive & PersistenceMongodb for Offers & update DB Entity #42

---
+++ src/main/java/es/upm/miw/betca_tpv_core/infrastructure/mongodb/daos/OfferReactive.java
@@ -0,0 +1,16 @@
+package es.upm.miw.betca_tpv_core.infrastructure.mongodb.daos;
+
+import es.upm.miw.betca_tpv_core.infrastructure.mongodb.entities.OfferEntity;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveSortingRepository;
+import reactor.core.publisher.Flux;
+
+public interface OfferReactive extends ReactiveSortingRepository<OfferEntity, String> {
+
+    @Query("{$and:[" // allow NULL: all elements
+            + "?#{ [0] == null ? {_id : {$ne:null}} : { reference : {$regex:[0], $options: 'i'} } },"
+            + "?#{ [1] == null ? {_id : {$ne:null}} : { description : {$regex:[1], $options: 'i'} } },"
+            + "] }")
+    Flux<OfferEntity> findByReferenceAndDescriptionNullSafe(String reference, String description);
+
+}
