cf00da431a636d4aa10aef5bb3c62b16ff2a0575
acalvom <andreac.3108@gmail.com>
2021-03-08 17:32:47 +0100

    implement test for "Create new Offer: Barcode not exists" in Persistence MongoDB #44

--- src/test/java/es/upm/miw/betca_tpv_core/infrastructure/mongodb/persistence/OfferPersistenceMongodbIT.java
+++ src/test/java/es/upm/miw/betca_tpv_core/infrastructure/mongodb/persistence/OfferPersistenceMongodbIT.java
@@ -3,22 +3,15 @@ package es.upm.miw.betca_tpv_core.infrastructure.mongodb.persistence;
 import es.upm.miw.betca_tpv_core.TestConfig;
 import es.upm.miw.betca_tpv_core.domain.exceptions.ConflictException;
 import es.upm.miw.betca_tpv_core.domain.exceptions.NotFoundException;
-import es.upm.miw.betca_tpv_core.domain.model.Article;
 import es.upm.miw.betca_tpv_core.domain.model.Offer;
-import es.upm.miw.betca_tpv_core.domain.model.Provider;
-import es.upm.miw.betca_tpv_core.infrastructure.api.dtos.OfferCreationEditionDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import reactor.test.StepVerifier;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
 
-import static java.math.BigDecimal.TEN;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestConfig
@@ -61,26 +54,17 @@ class OfferPersistenceMongodbIT {
                 .verify();
     }
 
-/*
-OfferCreationEditionDto newOffer = new OfferCreationEditionDto(null, "new offer",
-            LocalDate.of(2021, 9, 15), new BigDecimal("75"),
-            new String[]{"8400000000031", "8400000000024", "8400000000017"});
-    @Test
-    void testCreateExistingReference() {
-        StepVerifier
-                .create(this.offerPersistenceMongodb.create(
-                        Offer.builder().reference("cmVmZXJlbmNlb2ZmZXIy").description("error").build()))
-                .expectError(ConflictException.class)
-                .verify();
-    }
-
     @Test
     void testCreateNotExistingBarcode() {
         StepVerifier
                 .create(this.offerPersistenceMongodb.create(
-                        Offer.builder().reference("123").description("error")
-                                .articleBarcodeList(List.of("8400000000017", "hh")).build()))
+                        Offer.builder()
+                                .reference("cmVmZXJlbmNlb2ZmZXIy").description("barcode does not exist")
+                                .expiryDate(LocalDate.of(2021, Month.MARCH, 31))
+                                .discount(new BigDecimal("75"))
+                                .articleBarcodes(new String[]{"kk", "8400000000024", "8400000000017"})
+                                .build()))
                 .expectError(NotFoundException.class)
                 .verify();
-    }*/
+    }
 }
