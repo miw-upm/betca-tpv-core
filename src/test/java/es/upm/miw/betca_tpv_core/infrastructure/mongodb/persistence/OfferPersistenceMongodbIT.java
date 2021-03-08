12da867fa9a9d658a9927d8af7a94101211dee52
acalvom <andreac.3108@gmail.com>
2021-03-08 17:27:51 +0100

    implement test for "Create new Offer" in Persistence MongoDB #44

--- src/test/java/es/upm/miw/betca_tpv_core/infrastructure/mongodb/persistence/OfferPersistenceMongodbIT.java
+++ src/test/java/es/upm/miw/betca_tpv_core/infrastructure/mongodb/persistence/OfferPersistenceMongodbIT.java
@@ -6,18 +6,20 @@ import es.upm.miw.betca_tpv_core.domain.exceptions.NotFoundException;
 import es.upm.miw.betca_tpv_core.domain.model.Article;
 import es.upm.miw.betca_tpv_core.domain.model.Offer;
 import es.upm.miw.betca_tpv_core.domain.model.Provider;
+import es.upm.miw.betca_tpv_core.infrastructure.api.dtos.OfferCreationEditionDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import reactor.test.StepVerifier;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.math.BigDecimal.TEN;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestConfig
 class OfferPersistenceMongodbIT {
@@ -25,24 +27,31 @@ class OfferPersistenceMongodbIT {
     @Autowired
     private OfferPersistenceMongodb offerPersistenceMongodb;
 
-    /*@Test
+    @Test
     void testCreate() {
         StepVerifier
                 .create(this.offerPersistenceMongodb.create(
-                        Offer.builder().reference("abc").description("create")
-                                .expiryDate(LocalDateTime.of(2021, Month.MARCH, 31, 20, 20))
-                                .discount(new BigDecimal("50")).articleBarcodeList(List.of("8400000000017", "8400000000031"))
+                        Offer.builder().description("test create persistence")
+                                .expiryDate(LocalDate.of(2021, Month.MARCH, 31))
+                                .discount(new BigDecimal("75"))
+                                .articleBarcodes(new String[]{"8400000000031", "8400000000024", "8400000000017"})
                                 .build()))
                 .expectNextMatches(offer -> {
-                    assertEquals("abc", offer.getReference());
-                    assertEquals(new BigDecimal("50"), offer.getDiscount());
-                    assertTrue(offer.getArticleBarcodeList().contains("8400000000017"));
+                    assertNull(offer.getReference());
+                    assertEquals("test create persistence", offer.getDescription());
+                    assertEquals(new BigDecimal("75"), offer.getDiscount());
+                    assertNotNull(offer.getArticleBarcodes());
                     return true;
                 })
                 .expectComplete()
                 .verify();
     }
 
+
+/*
+OfferCreationEditionDto newOffer = new OfferCreationEditionDto(null, "new offer",
+            LocalDate.of(2021, 9, 15), new BigDecimal("75"),
+            new String[]{"8400000000031", "8400000000024", "8400000000017"});
     @Test
     void testCreateExistingReference() {
         StepVerifier
