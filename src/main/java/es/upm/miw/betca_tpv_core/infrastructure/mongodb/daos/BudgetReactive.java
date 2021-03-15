af925f7a9908bef8e82001104550651787f2591a
yicheng <yicheng.pan@alumnos.upm.es>
2021-03-15 17:15:46 +0100

    Add Budget Dao&Entity&Reactive and Test

---
+++ src/main/java/es/upm/miw/betca_tpv_core/infrastructure/mongodb/daos/BudgetReactive.java
@@ -0,0 +1,9 @@
+package es.upm.miw.betca_tpv_core.infrastructure.mongodb.daos;
+
+import es.upm.miw.betca_tpv_core.infrastructure.mongodb.entities.BudgetEntity;
+import org.springframework.data.repository.reactive.ReactiveSortingRepository;
+import reactor.core.publisher.Mono;
+
+public interface BudgetReactive extends ReactiveSortingRepository<BudgetEntity, String > {
+    Mono<BudgetEntity> findById(String id);
+}
