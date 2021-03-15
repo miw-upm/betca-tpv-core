406987863545c8cf1329be5e1d0a886b1451049c
manue <manuem07@ucm.es>
2021-03-15 20:01:50 +0100

    Create test #64

--- src/test/java/es/upm/miw/betca_tpv_core/infrastructure/api/resources/GiftTicketResourceIT.java
+++ src/test/java/es/upm/miw/betca_tpv_core/infrastructure/api/resources/GiftTicketResourceIT.java
@@ -1,24 +1,15 @@
 package es.upm.miw.betca_tpv_core.infrastructure.api.resources;
 
 import es.upm.miw.betca_tpv_core.domain.model.*;
-import es.upm.miw.betca_tpv_core.domain.rest.UserMicroservice;
 import es.upm.miw.betca_tpv_core.infrastructure.api.RestClientTestService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
-import java.math.BigDecimal;
-import java.util.List;
-
-import static es.upm.miw.betca_tpv_core.infrastructure.api.resources.CashierResource.CASHIERS;
 import static es.upm.miw.betca_tpv_core.infrastructure.api.resources.GiftTicketResource.GIFTTICKETS;
-import static es.upm.miw.betca_tpv_core.infrastructure.api.resources.TicketResource.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
 
 
 @RestTestConfig
@@ -28,43 +19,11 @@ public class GiftTicketResourceIT {
     private WebTestClient webTestClient;
     @Autowired
     private RestClientTestService restClientTestService;
-    @MockBean
-    private UserMicroservice userMicroservice;
 
     @Test
     void testCreate() {
-        this.restClientTestService.loginAdmin(webTestClient)
-                .post().uri(CASHIERS)
-                .exchange()
-                .expectStatus().isOk();
-        BDDMockito.given(this.userMicroservice.readByMobile(anyString()))
-                .willAnswer(arguments ->
-                        Mono.just(User.builder().mobile(arguments.getArgument(0)).firstName("mock").build()));
-
-        Shopping shopping1 = Shopping.builder().barcode("8400000000017").retailPrice(new BigDecimal("20")).amount(1)
-                .discount(BigDecimal.ZERO).state(ShoppingState.COMMITTED).build();
-        Ticket ticket = Ticket.builder().cash(new BigDecimal("200"))
-                .card(BigDecimal.ZERO).voucher(BigDecimal.ZERO).note("note")
-                .shoppingList(List.of(shopping1)).user(User.builder().mobile("666666004").build()).build();
-        Ticket dbTicket = this.restClientTestService.loginAdmin(webTestClient)
-                .post()
-                .uri(TICKETS)
-                .body(Mono.just(ticket), Ticket.class)
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(Ticket.class)
-                .value(Assertions::assertNotNull)
-                .value(returnTicket -> {
-                    System.out.println(">>>>> ticket: " + returnTicket + " total: " + returnTicket.total());
-                    assertNotNull(returnTicket.getId());
-                    assertNotNull(returnTicket.getReference());
-                    assertNotNull(returnTicket.getCreationDate());
-                    assertEquals(0, new BigDecimal("20.00").compareTo(returnTicket.total()));
-                }).returnResult().getResponseBody();
-        assertNotNull(dbTicket);
-
         GiftTicket giftTicket = GiftTicket.builder().message("description of GiftTicket")
-                .ticketId(dbTicket.getId()).build();
+                .ticketId("5fa45e863d6e834d642689ac").build();
         this.restClientTestService.loginAdmin(webTestClient)
                 .post()
                 .uri(GIFTTICKETS)
@@ -77,7 +36,7 @@ public class GiftTicketResourceIT {
                     System.out.println(">>>>> Test:: returnGiftTicket:" + returnGiftTicket);
                     assertNotNull(returnGiftTicket.getId());
                     assertEquals("description of GiftTicket", returnGiftTicket.getMessage());
-                    assertEquals(dbTicket.getId(), returnGiftTicket.getTicketId());
+                    assertEquals("5fa45e863d6e834d642689ac", returnGiftTicket.getTicketId());
                 });
     }
 
