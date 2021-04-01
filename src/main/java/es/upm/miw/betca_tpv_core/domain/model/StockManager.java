bbe3e9112633a445b39e28557a0207cbefb6a82e
Viviana Castillo <v.castillo@alumnos.upm.es>
2021-04-01 23:16:02 +0200

    Codesmell  #154

--- src/main/java/es/upm/miw/betca_tpv_core/domain/model/StockManager.java
+++ src/main/java/es/upm/miw/betca_tpv_core/domain/model/StockManager.java
@@ -1,10 +1,8 @@
 package es.upm.miw.betca_tpv_core.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import java.math.BigDecimal;
