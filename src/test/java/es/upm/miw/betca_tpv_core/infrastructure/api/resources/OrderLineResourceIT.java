f48f7333d840eee547abfbf05dfb65c0f99461d8
mohammed.bendohhou.laouini@alumnos.upm.es <mohammed.bendohhou.laouini@alumnos.upm.es>
2021-04-04 01:41:39 +0200

    Addition Test Post Order and OrderLine #175

--- src/test/java/es/upm/miw/betca_tpv_core/infrastructure/api/resources/OrderLineResourceIT.java
+++ src/test/java/es/upm/miw/betca_tpv_core/infrastructure/api/resources/OrderLineResourceIT.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
 import static es.upm.miw.betca_tpv_core.infrastructure.api.resources.OrderLineResource.ID;
 import static es.upm.miw.betca_tpv_core.infrastructure.api.resources.OrderLineResource.ORDER_LINES;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @RestTestConfig
 class OrderLineResourceIT {
@@ -39,6 +41,37 @@ class OrderLineResourceIT {
         this.restClientTestService.loginAdmin(webTestClient)
                 .get()
                 .uri(ORDER_LINES + ID, "100")
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    void testCreate() {
+
+        OrderLine orderLine = OrderLine.builder().articleBarcode("8400000000017").requireAmount(10).finalAmount(15).build();
+        OrderLine orderLineDB = this.restClientTestService.loginAdmin(webTestClient)
+                .post()
+                .uri(ORDER_LINES)
+                .body(Mono.just(orderLine), OrderLine.class)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(OrderLine.class)
+                .value(Assertions::assertNotNull)
+                .value(outOrderLine -> {
+                    assertEquals("8400000000017", outOrderLine.getArticleBarcode());
+                    assertEquals(10, outOrderLine.getRequireAmount());
+                    assertEquals(15, outOrderLine.getFinalAmount());
+                }).returnResult().getResponseBody();
+        assertNotNull(orderLineDB);
+    }
+
+    @Test
+    void testCreateNotExistBarcode() {
+        OrderLine orderLine = OrderLine.builder().articleBarcode("9900000000017").requireAmount(10).finalAmount(15).build();
+        this.restClientTestService.loginAdmin(webTestClient)
+                .post()
+                .uri(ORDER_LINES)
+                .body(Mono.just(orderLine), OrderLine.class)
                 .exchange()
                 .expectStatus().isNotFound();
     }
