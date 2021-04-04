7211ce599f1c152646a7d82f381de0636ebaa08f
OdiseoSSJ <ulises.mateo.ortega@alumnos.upm.es>
2021-04-04 15:47:32 +0200

    Moving testFindTop5ArticleSalesLastWeek from ArticleResourceIT to proper layer ArticleServiceIT #54

---
+++ src/test/java/es/upm/miw/betca_tpv_core/domain/services/ArticleServiceIT.java
@@ -0,0 +1,20 @@
+package es.upm.miw.betca_tpv_core.domain.services;
+
+import es.upm.miw.betca_tpv_core.TestConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.test.StepVerifier;
+
+@TestConfig
+public class ArticleServiceIT {
+
+    @Autowired
+    private ArticleService articleService;
+
+    @Test
+    void testFindTop5ArticleSalesLastWeek() {
+        StepVerifier.create(articleService.findTop5ArticleSalesLastWeek())
+                    .expectComplete()
+                    .verify();
+    }
+}
