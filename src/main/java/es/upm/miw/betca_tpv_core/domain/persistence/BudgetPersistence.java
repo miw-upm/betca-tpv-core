114cd64d159ed5c2eee9eefdcacd9964f5c56650
Miguel Ángel <madelfresno@gmail.com>
2025-02-23 21:09:09 +0100

    Model, entity, persistence and test #589

---
+++ src/main/java/es/upm/miw/betca_tpv_core/domain/persistence/BudgetPersistence.java
@@ -0,0 +1,10 @@
+package es.upm.miw.betca_tpv_core.domain.persistence;
+
+import es.upm.miw.betca_tpv_core.domain.model.Budget;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface BudgetPersistence {
+    Mono<Budget> create(Budget budget);
+}
---
+++ src/main/java/es/upm/miw/betca_tpv_core/infrastructure/mongodb/daos/BudgetReactive.java
@@ -0,0 +1,7 @@
+package es.upm.miw.betca_tpv_core.infrastructure.mongodb.daos;
+
+import es.upm.miw.betca_tpv_core.infrastructure.mongodb.entities.BudgetEntity;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface BudgetReactive extends ReactiveMongoRepository<BudgetEntity, String> {
+}
