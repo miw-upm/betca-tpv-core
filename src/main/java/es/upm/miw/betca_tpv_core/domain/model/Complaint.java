9ae693d3ece80b6d8a842a557686d1e6017892a5
jorgempavon <jorge.mpavon@alumnos.upm.es>
2025-02-26 22:17:00 +0100

    added complaint class

---
+++ src/main/java/es/upm/miw/betca_tpv_core/domain/model/Complaint.java
@@ -0,0 +1,37 @@
+package es.upm.miw.betca_tpv_core.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Complaint {
+
+    @NotBlank
+    private String barcode;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") // pattern="dd/MM/yyyy hh:mm" o iso = DateTimeFormat.ISO.TIME
+    private LocalDateTime registrationDate;
+
+    @NotBlank
+    private String description;
+
+    private String reply;
+
+    @NotBlank
+    private Number mobile;
+
+    @Enumerated(EnumType.STRING)
+    private ComplaintState state;
+}
