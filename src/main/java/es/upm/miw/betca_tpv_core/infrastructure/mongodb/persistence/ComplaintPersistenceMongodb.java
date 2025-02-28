715e4c4b3150ddf8c7ae47520494f37637436f09
jorgempavon <jorge.mpavon@alumnos.upm.es>
2025-02-28 17:47:00 +0100

    fixed persistence

--- src/main/java/es/upm/miw/betca_tpv_core/infrastructure/mongodb/persistence/ComplaintPersistenceMongodb.java
+++ src/main/java/es/upm/miw/betca_tpv_core/infrastructure/mongodb/persistence/ComplaintPersistenceMongodb.java
@@ -4,6 +4,7 @@ import es.upm.miw.betca_tpv_core.domain.model.Complaint;
 import es.upm.miw.betca_tpv_core.domain.persistence.ComplaintPersistence;
 import es.upm.miw.betca_tpv_core.infrastructure.mongodb.daos.ComplaintReactive;
 import es.upm.miw.betca_tpv_core.infrastructure.mongodb.entities.ComplaintEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
@@ -12,6 +13,7 @@ public class ComplaintPersistenceMongodb implements ComplaintPersistence {
 
     private final ComplaintReactive complaintReactive;
 
+    @Autowired
     public ComplaintPersistenceMongodb(ComplaintReactive complaintReactive){
         this.complaintReactive=complaintReactive;
     }
