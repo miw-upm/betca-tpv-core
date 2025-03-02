1cdd6b32c2de9a14ee076ed9c803eaee941ee93a
joralvmel <joralvmel@gmail.com>
2025-03-02 20:16:35 +0100

    miw-upm/betca-tpv#610 update customer points tests

--- src/test/java/es/upm/miw/betca_tpv_core/domain/model/CustomerPointsTest.java
+++ src/test/java/es/upm/miw/betca_tpv_core/domain/model/CustomerPointsTest.java
@@ -1,13 +1,14 @@
 package es.upm.miw.betca_tpv_core.domain.model;
 
-
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class CustomerPointsTest {
+
     @Test
     void testBuilder() {
         LocalDateTime now = LocalDateTime.now();
@@ -16,5 +17,25 @@ class CustomerPointsTest {
         assertEquals(10, customerPoints.getValue());
         assertEquals(now, customerPoints.getLastDate());
         assertEquals(user, customerPoints.getUser());
+    }
+
+    @Test
+    void testSettersAndGetters() {
+        LocalDateTime now = LocalDateTime.now();
+        User user = User.builder().mobile("666666666").build();
+        CustomerPoints customerPoints = new CustomerPoints();
+        customerPoints.setValue(20);
+        customerPoints.setLastDate(now);
+        customerPoints.setUser(user);
+
+        assertEquals(20, customerPoints.getValue());
+        assertEquals(now, customerPoints.getLastDate());
+        assertEquals(user, customerPoints.getUser());
+    }
+
+    @Test
+    void testNoArgsConstructor() {
+        CustomerPoints customerPoints = new CustomerPoints();
+        assertNotNull(customerPoints);
     }
 }
