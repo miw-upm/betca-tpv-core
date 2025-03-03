f4585a00789b3f5e5d48635b7c8c9fa5865c92b4
jorgempavon <jorge.mpavon@alumnos.upm.es>
2025-03-04 00:02:26 +0100

    defined test like admin and customer for search  endpoint

--- src/test/java/es/upm/miw/betca_tpv_core/infrastructure/api/resources/ComplaintResourceIT.java
+++ src/test/java/es/upm/miw/betca_tpv_core/infrastructure/api/resources/ComplaintResourceIT.java
@@ -19,7 +19,7 @@ class ComplaintResourceIT {
     private RestClientTestService restClientTestService;
 
     @Test
-    void testFindByUserMobileNullSafe_MobileWithComplaintsAssociated(){
+    void testFindByUserMobileNullSafe_MobileWithComplaintsAssociated_LoggedLikeAdmin(){
         this.restClientTestService.loginAdmin(webTestClient)
                 .get()
                 .uri(uriBuilder -> uriBuilder
@@ -38,7 +38,7 @@ class ComplaintResourceIT {
     }
 
     @Test
-    void testFindByUserMobileNullSafe_MobileWithNoComplaintsAssociated(){
+    void testFindByUserMobileNullSafe_MobileWithNoComplaintsAssociated_LoggedLikeAdmin(){
         this.restClientTestService.loginAdmin(webTestClient)
                 .get()
                 .uri(uriBuilder -> uriBuilder
@@ -57,7 +57,7 @@ class ComplaintResourceIT {
     }
 
     @Test
-    void testFindByUserMobileNullSafe_NullMobile(){
+    void testFindByUserMobileNullSafe_NullMobile_LoggedLikeAdmin(){
         this.restClientTestService.loginAdmin(webTestClient)
                 .get()
                 .uri(COMPLAINTS+SEARCH)
@@ -68,5 +68,49 @@ class ComplaintResourceIT {
                 .value( complaints -> assertTrue( (long) complaints
                         .size() >=3)
                 );
+    }
+
+    @Test
+    void testFindByUserMobileNullSafe_MobileWithComplaintsAssociated_LoggedLikeCustomer(){
+        this.restClientTestService.loginCustomer(webTestClient)
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(COMPLAINTS+SEARCH)
+                        .queryParam("userMobile","66")
+                        .build()
+
+                )
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Complaint.class)
+                .value(Assertions::assertNotNull)
+                .value( complaints -> assertTrue( (long) complaints
+                        .size() >=3)
+                );
+    }
+
+    @Test
+    void testFindByUserMobileNullSafe_Forbidden_LoggedLikeCustomer(){
+        this.restClientTestService.loginCustomer(webTestClient)
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(COMPLAINTS+SEARCH)
+                        .queryParam("userMobile","6")
+                        .build()
+
+                )
+                .exchange()
+                .expectStatus()
+                .isUnauthorized();
+    }
+
+    @Test
+    void testFindByUserMobileNullSafe_ForbiddenNull_LoggedLikeCustomer(){
+        this.restClientTestService.loginCustomer(webTestClient)
+                .get()
+                .uri(COMPLAINTS+SEARCH)
+                .exchange()
+                .expectStatus()
+                .isUnauthorized();
     }
 }
