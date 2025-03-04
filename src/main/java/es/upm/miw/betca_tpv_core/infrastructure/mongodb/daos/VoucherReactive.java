4f361fd7bedaf0a490adb60aeeeeee54a9e7c0b8
Martxel <56430705+MartxelAran@users.noreply.github.com>
2025-03-04 18:35:37 +0100

    Add search by reference and value miw-upm/betca-tpv#617

--- src/main/java/es/upm/miw/betca_tpv_core/infrastructure/mongodb/daos/VoucherReactive.java
+++ src/main/java/es/upm/miw/betca_tpv_core/infrastructure/mongodb/daos/VoucherReactive.java
@@ -1,10 +1,20 @@
 package es.upm.miw.betca_tpv_core.infrastructure.mongodb.daos;
 
 import es.upm.miw.betca_tpv_core.infrastructure.mongodb.entities.VoucherEntity;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
 
 public interface VoucherReactive extends ReactiveMongoRepository<VoucherEntity, String> {
     Mono<VoucherEntity> findByReference(String reference);
     Mono<VoucherEntity> findByUserMobile(String userMobile);
+    @Query("{$and:["
+            + "?#{ [0] == null ? {_id : {$ne:null}} : { reference : {$regex:[0], $options: 'i'} } },"
+            + "?#{ [1] == null ? {_id : {$ne:null}} : { value : {$lt:[1]} } },"
+            + "] }")
+    Flux<VoucherEntity> findByReferenceAndValueNullSafe(
+            String reference, BigDecimal value);
 }
