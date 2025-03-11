c7238c084dc145da4149bcab368e5f475d43c28b
jorgempavon <jorge.mpavon@alumnos.upm.es>
2025-03-12 00:07:48 +0100

    modified PreAuthorize in create Complaint Resource miw-upm/betca-tpv#637

--- src/main/java/es/upm/miw/betca_tpv_core/infrastructure/api/resources/ComplaintResource.java
+++ src/main/java/es/upm/miw/betca_tpv_core/infrastructure/api/resources/ComplaintResource.java
@@ -25,7 +25,7 @@ public class ComplaintResource {
         this.complaintService=complaintService;
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN','MANAGER','OPERATOR') or #userMobile == authentication.principal")
+    @PreAuthorize("hasRole('CUSTOMER') or #userMobile == authentication.principal")
     @PostMapping(produces = {"application/json"})
     public Mono<Complaint> create(@RequestBody ComplaintCreationDto complaintCreationDto){
         return this.complaintService.create(complaintCreationDto.toComplaint());
