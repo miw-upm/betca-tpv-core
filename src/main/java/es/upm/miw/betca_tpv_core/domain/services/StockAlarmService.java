d707609f91216656f8ac437d919729edea1001c8
TrueAlexCv <alexcvtrue24282@gmail.com>
2025-03-11 15:49:00 +0100

    Create stock alarm miw-upm/betca-tpv#635

---
+++ src/main/java/es/upm/miw/betca_tpv_core/domain/services/StockAlarmService.java
@@ -0,0 +1,20 @@
+package es.upm.miw.betca_tpv_core.domain.services;
+
+import es.upm.miw.betca_tpv_core.domain.model.StockAlarm;
+import es.upm.miw.betca_tpv_core.domain.persistence.StockAlarmPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Mono;
+
+public class StockAlarmService {
+
+    private final StockAlarmPersistence stockAlarmPersistence;
+
+    @Autowired
+    public StockAlarmService(StockAlarmPersistence stockAlarmPersistence) {
+        this.stockAlarmPersistence = stockAlarmPersistence;
+    }
+
+    public Mono<StockAlarm> create(StockAlarm stockAlarm) {
+        return this.stockAlarmPersistence.create(stockAlarm);
+    }
+}
