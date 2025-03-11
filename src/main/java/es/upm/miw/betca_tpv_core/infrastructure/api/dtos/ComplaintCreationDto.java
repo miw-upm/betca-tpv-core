48fa5702f212953a6edeb202bbf6dd0f74bd1a83
jorgempavon <jorge.mpavon@alumnos.upm.es>
2025-03-11 22:43:39 +0100

    defined ComplaintCreationDto miw-upm/betca-tpv#637

--- src/main/java/es/upm/miw/betca_tpv_core/infrastructure/api/dtos/ComplaintCreationDto.java
+++ src/main/java/es/upm/miw/betca_tpv_core/infrastructure/api/dtos/ComplaintCreationDto.java
@@ -1,8 +1,22 @@
 package es.upm.miw.betca_tpv_core.infrastructure.api.dtos;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import es.upm.miw.betca_tpv_core.domain.model.Complaint;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanUtils;
 
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ComplaintCreationDto {
 
     @NotBlank
@@ -11,10 +25,18 @@ public class ComplaintCreationDto {
     @NotBlank
     @NotNull
     private String description;
+    @NotNull
+    @NotBlank
+    private String userMobile;
 
+    public Complaint toComplaint(){
+        Complaint complaint = new Complaint();
+        BeanUtils.copyProperties(this,complaint);
+        complaint.setState("CLOSED");
+        complaint.setReply("");
+        complaint.setRegistrationDate(LocalDateTime.now());
 
-
-
-
+        return complaint;
+    }
 
 }
