158aa98972a19333918860c2794c285f2db0afff
jorgempavon <jorge.mpavon@alumnos.upm.es>
2025-03-11 16:08:56 +0100

    defined create Complaint persistence mongodb miw-upm/betca-tpv#637

--- src/main/java/es/upm/miw/betca_tpv_core/infrastructure/mongodb/persistence/ComplaintPersistenceMongodb.java
+++ src/main/java/es/upm/miw/betca_tpv_core/infrastructure/mongodb/persistence/ComplaintPersistenceMongodb.java
@@ -1,13 +1,16 @@
 package es.upm.miw.betca_tpv_core.infrastructure.mongodb.persistence;
 
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import es.upm.miw.betca_tpv_core.domain.exceptions.ForbiddenException;
 import es.upm.miw.betca_tpv_core.domain.exceptions.NotFoundException;
 import es.upm.miw.betca_tpv_core.domain.model.Complaint;
 import es.upm.miw.betca_tpv_core.domain.model.User;
 import es.upm.miw.betca_tpv_core.domain.persistence.ComplaintPersistence;
+import es.upm.miw.betca_tpv_core.infrastructure.mongodb.daos.ArticleReactive;
 import es.upm.miw.betca_tpv_core.infrastructure.mongodb.daos.ComplaintReactive;
 import es.upm.miw.betca_tpv_core.infrastructure.mongodb.entities.ComplaintEntity;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -18,14 +21,27 @@ public class ComplaintPersistenceMongodb implements ComplaintPersistence {
 
     private final ComplaintReactive complaintReactive;
 
+    private final ArticleReactive articleReactive;
+
     @Autowired
-    public ComplaintPersistenceMongodb(ComplaintReactive complaintReactive){
+    public ComplaintPersistenceMongodb(ComplaintReactive complaintReactive,ArticleReactive articleReactive){
         this.complaintReactive=complaintReactive;
+        this.articleReactive=articleReactive;
     }
 
     @Override
     public Mono<Complaint> create(Complaint complaint) {
-        return this.complaintReactive.create(complaint);
+        ComplaintEntity complaintEntity = new ComplaintEntity();
+        BeanUtils.copyProperties(complaint,complaintEntity);
+
+        return this.articleReactive.findByBarcode(complaint.getBarcode())
+                .switchIfEmpty(Mono.error(new NotFoundException("The article does not exist with the barcode provided.")))
+                .map(articleEntity -> {
+                    complaintEntity.setArticle(articleEntity);
+                    return complaintEntity;
+                })
+                .flatMap(this.complaintReactive::save)
+                .map(ComplaintEntity::toComplaint);
     }
 
     @Override
