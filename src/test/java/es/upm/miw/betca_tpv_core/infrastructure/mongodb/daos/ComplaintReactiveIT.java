0cb851c2ada09d3fa731282cbd73ca77fa6ab6cc
jorgempavon <jorge.mpavon@alumnos.upm.es>
2025-03-13 23:32:57 +0100

    added tests in ComplaintReactiveIT miw-upm/betca-tpv#637

--- src/test/java/es/upm/miw/betca_tpv_core/infrastructure/mongodb/daos/ComplaintReactiveIT.java
+++ src/test/java/es/upm/miw/betca_tpv_core/infrastructure/mongodb/daos/ComplaintReactiveIT.java
@@ -1,10 +1,17 @@
 package es.upm.miw.betca_tpv_core.infrastructure.mongodb.daos;
 
 import es.upm.miw.betca_tpv_core.TestConfig;
+import es.upm.miw.betca_tpv_core.domain.model.Article;
+import es.upm.miw.betca_tpv_core.domain.model.Provider;
+import es.upm.miw.betca_tpv_core.infrastructure.mongodb.entities.ArticleEntity;
 import es.upm.miw.betca_tpv_core.infrastructure.mongodb.entities.ComplaintState;
+import es.upm.miw.betca_tpv_core.infrastructure.mongodb.entities.ProviderEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import reactor.test.StepVerifier;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,6 +19,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class ComplaintReactiveIT {
     @Autowired
     public ComplaintReactive complaintReactive;
+    @Autowired
+    public ArticleReactive articleReactive;
 
     @Test
     void testFindByUserMobileNullSafe_MobileWithComplaintsAssociated(){
@@ -46,6 +55,27 @@ class ComplaintReactiveIT {
                 .create(this.complaintReactive.findByUserMobileNullSafe(null))
                 .assertNext(complaint -> assertNotNull(complaint.getDescription()))
                 .thenCancel()
+                .verify();
+    }
+    @Test
+    void testFindByUserMobileAndBarcode_NotExistsComplaint() {
+        StepVerifier
+                .create(
+                        articleReactive.findByBarcode("8400000000100")
+                                .flatMap(article -> complaintReactive.findByUserMobileAndArticle("66", article))
+                )
+                .expectComplete()
+                .verify();
+    }
+    @Test
+    void testFindByUserMobileAndBarcode_ExistsComplaint() {
+        StepVerifier
+                .create(
+                        articleReactive.findByBarcode("8400000000017")
+                                .flatMap(article -> complaintReactive.findByUserMobileAndArticle("66", article))
+                )
+                .expectNextMatches(complaint -> complaint != null && complaint.getArticle().getBarcode().equals("8400000000017") && complaint.getUserMobile().equals("66"))
+                .expectComplete()
                 .verify();
     }
 }
