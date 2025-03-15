de1f058520e846248dac2a53c91efff4389a5276
jorgempavon <jorge.mpavon@alumnos.upm.es>
2025-03-15 11:38:45 +0100

    fixed conflicts in test  miw-upm/betca-tpv#637

--- src/test/java/es/upm/miw/betca_tpv_core/domain/services/ComplaintServiceIT.java
+++ src/test/java/es/upm/miw/betca_tpv_core/domain/services/ComplaintServiceIT.java
@@ -67,7 +67,7 @@ class ComplaintServiceIT {
                 .build();
 
         StepVerifier
-                .create(this.complaintService.create(complaint))
+                .create(this.complaintService.create(complaint),authentication)
                 .assertNext(complaint1 -> complaint1.getDescription().contains("Queja de cliente enfadado"))
                 .thenCancel()
                 .verify();
@@ -100,7 +100,7 @@ class ComplaintServiceIT {
                 .build();
 
         StepVerifier
-                .create(this.complaintService.create(complaint))
+                .create(this.complaintService.create(complaint,authentication))
                 .expectError(NotFoundException.class)
                 .verify();
     }
