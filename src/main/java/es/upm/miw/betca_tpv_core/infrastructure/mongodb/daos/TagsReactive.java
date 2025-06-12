c615fc5b2266a9b61995e0bd6c9a32cfc9eefc35
Ivan Esteban Vargas Martinez <31573852+IvanEsteban94@users.noreply.github.com>
2025-06-12 02:27:39 +0200

    Update TagsReactive.java

--- src/main/java/es/upm/miw/betca_tpv_core/infrastructure/mongodb/daos/TagsReactive.java
+++ src/main/java/es/upm/miw/betca_tpv_core/infrastructure/mongodb/daos/TagsReactive.java
@@ -7,21 +7,9 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface TagsReactive extends ReactiveMongoRepository<TagsEntity, String> {
-    Mono<TagsEntity> findByName(String name);
-
-    @Query("{$and:[" // allow NULL: all elements
-            + "?#{ [0] == null ? {_id : {$ne:null}} : { name : {$regex:[0], $options: 'i'} } },"
-            + "?#{ [1] == null ? {_id : {$ne:null}} : { group : {$regex:[1], $options: 'i'} } },"
-            + "?#{ [2] == null ? {_id : {$ne:null}} : { description : {$regex:[2], $options: 'i'} } }"
-            + "] }")
-    Flux<TagsEntity> findByNameAndGroupAndDescriptionNullSafe(
-            String name, String group, String description);
-
-    Flux<TagsEntity> findByGroupIsNull();
-
-    @Query("{$and:[" // allow NULL in name
-            + "?#{ [0] == null ? {_id : {$ne:null}} : { name : {$regex:[0], $options: 'i'} } },"
-            + "{group : {$ne : null}}"
-            + "] }")
-    Flux<TagsEntity> findByNameLikeAndGroupIsNotNullNullSafe(String name);
+  Mono<TagEntity> findById(String id);
+    
+    Flux<TagEntity> findByName(String name);
+    
+    Flux<TagEntity> findByGroup(String group);
 }
